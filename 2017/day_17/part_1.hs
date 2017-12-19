6799230394c291983b2c55b5915c44388b66dcf4
Hsekiu <kpetr7@gmail.com>
2017-12-18 22:09:56 -0800

    Added solution part 1 day 17.

--- 2017/day_17/part_1.hs
+++ 2017/day_17/part_1.hs
@@ -1,8 +1,17 @@
 import Debug.Trace
+import Data.Char
+import Data.List
+import Data.Maybe
 
 spin :: [Int] -> Int -> Int -> [Int] 
-spin x y z | y == 3 = x
-           | otherwise = trace (show x ++ " : " ++ show newpos) $ spin (insert (succ y) newpos x) (succ y) newpos
+spin x y z | y == 2017 = x
+           | otherwise = spin (insert (succ y) newpos x) (succ y) (newpos + 1)
     where insert num index = (\x -> (fst (split index)) ++ [num] ++ (snd (split index)))
           split index = splitAt (index + 1) x
-          newpos = if (z + 3) < (length x) then z + 3 else (z + 3) `mod` length x
+          newpos = (z + 343) `mod` length x
+
+findposition ele = (\(Just i)->i) . findIndex (==ele)
+
+main = do
+    let buffer = spin [0] 0 0
+    print (buffer !! ((findposition 2017 buffer) + 1))
