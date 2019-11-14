b057edbe53a5bdc3f5bc6b2b7b24a300db40b7f6
Hsekiu <kpetr7@gmail.com>
2019-11-13 17:58:52 -0800

    Added solution day 4

---
+++ 2015/day_4/part_2.hs
@@ -0,0 +1,11 @@
+import Data.Hash.MD5
+
+--Determine if first 5/6 leading zero's of hash are 0's
+hash :: String -> Int -> Bool
+hash x y = if (take 6 (md5s (Str (x ++ show y)))) == "000000" then True else False
+
+findNum :: String -> Int -> Int
+findNum x y = if (hash x y) == False then findNum x (y + 1) else y
+
+main = do
+   print (findNum "bgvyzdsv" 0)
